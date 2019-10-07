5b4183ba9443fcece05a14fef459f7824be2a656
Paul <pavelprfnt@gmail.com>
2019-10-07 21:58:31 +0300

    Первый коммит

---
+++ Udemy practive/script1.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var letsPractice= 5;
+var hereName = "Fedor";
+let Boom = 5;
+const pi= 3.14;
+
+var newObj = {
+    name: "Vasya",
+    surname: "Pupkin",
+    Age: 35,
+    Status: "married"
+}
+console.log(newObj["Age"]);
+
+// let arr =[ 'notch', 'gotch', 'dotch']
+// console.log(arr[0]);
+
+// alert('Hello World!');
+// let jim = confirm("are you here?");
+// console.log(jim); 
+let jim = prompt("are you here?", "Yes");
+console.log(jim); 
